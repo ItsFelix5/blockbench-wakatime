c3a16c0d76659e963edcfc5056dc4c25e562a634
itsfelix5 <felixsoetewey@outlook.com>
2025-07-19 15:14:48 +0200

    Fix improper stealing of code from which I stole

--- wakatime.js
+++ wakatime.js
@@ -24,6 +24,7 @@
 		repository: "https://github.com/ItsFelix5/blockbench-wakatime",
 		onload: async function () {
 			wakatimeDiv = document.createElement("div");
+			wakatimeDiv.innerHTML = "Wakatime";
 			wakatimeDiv.style.cursor = "pointer";
 			wakatimeDiv.onclick = () => askProjectName(() => sendHeartBeat());
 			document.getElementById("status_bar").appendChild(wakatimeDiv);
@@ -36,9 +37,26 @@
 					return;
 				}
 
-				const latestVersion = (await response.json()).tag_name;
-				const cli = getCliLocation();
-				if (latestVersion) child_process.execFile(
+				const latestVersion = (await response.json())["tag_name"];
+				if (!latestVersion) throw "Tag name is " + latestVersion;
+
+				const resourcesLocation = path.join(getHomeDirectory(), ".wakatime");
+				fs.mkdirSync(resourcesLocation, {recursive: true});
+				const platform = osArch();
+				if (![
+					"darwin-amd64", "darwin-arm64",
+					"freebsd-386", "freebsd-amd64", "freebsd-arm",
+					"linux-386", "linux-amd64", "linux-arm", "linux-arm64",
+					"netbsd-386", "netbsd-amd64", "netbsd-arm",
+					"openbsd-386", "openbsd-amd64", "openbsd-arm", "openbsd-arm64",
+					"windows-386", "windows-amd64", "windows-arm64",
+				].includes(platform)) console.error(`Unsupported platform: ${platform}`);
+				const cli = path.join(
+					resourcesLocation,
+					`wakatime-cli-${platform}${isWindows() ? ".exe" : ""}`
+				);
+
+				child_process.execFile(
 					cli,
 					["--version"],
 					(error, stdout) => {
@@ -51,19 +69,10 @@
 								return;
 							} else console.log(`Found an updated wakatime-cli ${latestVersion}`);
 						} else console.error(error);
-						const osArch = (isWindows() ? "windows" : process.platform) + '-' + architecture();
 
-						if (![
-							"darwin-amd64", "darwin-arm64",
-							"freebsd-386", "freebsd-amd64", "freebsd-arm",
-							"linux-386", "linux-amd64", "linux-arm", "linux-arm64",
-							"netbsd-386", "netbsd-amd64", "netbsd-arm",
-							"openbsd-386", "openbsd-amd64", "openbsd-arm", "openbsd-arm64",
-							"windows-386", "windows-amd64", "windows-arm64",
-						].includes(osArch)) console.error(`Unsupported platform: ${osArch}`);
-						const url = `https://github.com/wakatime/wakatime-cli/releases/download/${version}/wakatime-cli-${osArch}.zip`;
+						const url = `https://github.com/wakatime/wakatime-cli/releases/download/${latestVersion}/wakatime-cli-${platform}.zip`;
 						console.log(`Downloading wakatime-cli from ${url}`);
-						const zipFile = path.join(getResourcesLocation(), "wakatime-cli.zip");
+						const zipFile = path.join(resourcesLocation, "wakatime-cli.zip");
 						const file = fs.createWriteStream(zipFile);
 						fetch(url).then((r) => {
 							r.body.pipe(file);
@@ -71,40 +80,39 @@
 								console.log("Extracting wakatime-cli.zip file...");
 
 								try {
-									await del([cli], {force: true});
+									fs.rmSync(cli, {force: true});
 								} catch (e) {
-									log.warn(e);
+									console.warn(e);
 								}
-								await unzip(zipFile, getResourcesLocation());
+								await unzip(zipFile, resourcesLocation);
 
 								try {
 									console.log("Chmod 755 wakatime-cli...");
 									fs.chmodSync(cli, 0o755);
 								} catch (e) {
-									log.warn(e);
+									console.warn(e);
 								}
-								const link = path.join(getResourcesLocation(), `wakatime-cli${isWindows() ? ".exe" : ""}`);
+								const link = path.join(resourcesLocation, `wakatime-cli${isWindows() ? ".exe" : ""}`);
 								try {
 									if (fs.lstatSync(link).isSymbolicLink()) return;
 								} catch (_) {
 								}
 								try {
-									log.debug(`Create symlink from wakatime-cli to ${cli}`);
+									console.debug(`Create symlink from wakatime-cli to ${cli}`);
 									fs.symlinkSync(cli, link);
 								} catch (e) {
-									log.warn(e);
+									console.warn(e);
 									try {
 										fs.copyFileSync(cli, link);
 										fs.chmodSync(link, 0o755);
 									} catch (e2) {
-										log.warn(e2);
+										console.warn(e2);
 									}
 								}
 							}));
 						});
 					}
 				);
-				else console.log("Unable to find latest wakatime-cli version.");
 			} catch (error) {
 				console.warn("Fetch error:", error);
 			}
@@ -162,7 +170,10 @@
 		if (write) args.push("--write");
 
 		console.log(`Sending ${write ? "write" : ""} heartbeat for ${entity} in ${projectName}`);
-		const cli = getCliLocation();
+		const cli = path.join(
+			getHomeDirectory(), ".wakatime",
+			`wakatime-cli-${osArch()}${isWindows() ? ".exe" : ""}`
+		);
 		let proc = child_process.execFile(
 			cli,
 			args,
@@ -257,122 +268,47 @@
 		return process.platform === "win32";
 	}
 
-	function architecture() {
-		return process.arch.includes("arm") ? process.arch : process.arch.includes("32") ? "386" : "amd64";
+	function osArch() {
+		return (isWindows() ? "windows" : process.platform) + '-' + (process.arch.includes("arm") ? process.arch : process.arch.includes("32") ? "386" : "amd64");
 	}
 
 	async function unzip(zipFile, dest) {
-		if (fs.existsSync(zipFile)) {
-			const zip = fs.readFileSync(zipFile).buffer;
-			try {
-				const ua = new Uint8Array(zip);
-				const dv = new DataView(zip);
-				const offEOCD = ua.findLastIndex(
-					(e, i, a) =>
-						e === 0x50 &&
-						a[i + 1] === 0x4b &&
-						a[i + 2] === 0x05 &&
-						a[i + 3] === 0x06
-				);
-				const offCenDir = dv.getUint32(offEOCD + 16, true);
-				const recordCount = dv.getUint16(offEOCD + 10, true);
-				const parsedZip = {
-					buffer: zip,
-					array: ua,
-					view: dv,
-					eocdOffset: offEOCD,
-					centralDirOffset: offCenDir,
-					fileCount: recordCount,
-					files: {},
-				};
-				for (let i = 0, o = offCenDir; i < recordCount; i++) {
-					const n = dv.getUint16(o + 28, true);
-					const m = dv.getUint16(o + 30, true);
-					const k = dv.getUint16(o + 32, true);
-					const encodedPath = ua.subarray(o + 46, o + 46 + n);
-					const filePath = td.decode(encodedPath);
-
-					const h = dv.getUint32(o + 42, true);
-					const q = dv.getUint16(h + 8, true);
-					const t = dv.getUint16(h + 10, true);
-					const d = dv.getUint16(h + 12, true);
-					const s = dv.getUint32(o + 20, true);
-					const a = dv.getUint32(o + 24, true);
-					const e = dv.getUint16(h + 28, true);
-
-					parsedZip.files[filePath] = {
-						path: filePath,
-						compressedSize: s,
-						size: a,
-						crc32: dv.getUint32(o + 16, true),
-						timeValue: t,
-						dateValue: d,
-						encodedPath,
-						compressionMethod: q,
-						isDirectory: filePath.endsWith("/"),
-						compressedContent: ua.subarray(
-							h + 30 + n + e,
-							h + 30 + n + e + s
-						),
-					};
-
-					if (q === 0) parsedZip.files[filePath].content = parsedZip.files[filePath].compressedContent;
-					else Object.defineProperty(
-						parsedZip.files[filePath],
-						"content",
-						{
-							configurable: true,
-							enumerable: true,
-							get() {
-								const c = zlib.inflateRawSync(
-									this.compressedContent
-								);
-								Object.defineProperty(this, "content", {
-									value: c,
-									configurable: true,
-									enumerable: true,
-								});
-								return c;
-							},
-						}
-					);
-
-					o += 46 + n + m + k;
-				}
-
-				for (const file of Object.values(parsedZip.files)) {
-					if (file.isDirectory) continue;
-					const filePath = path.join(dest, file.path);
-					fs.mkdirSync(path.dirname(filePath), {recursive: true});
-					fs.writeFileSync(filePath, file.content);
-				}
-			} catch (e) {
-				console.error(e);
-			}
-		}
-	}
-
-	function getResourcesLocation() {
-		let resourcesLocation = path.join(getHomeDirectory(), ".wakatime");
+		if (!fs.existsSync(zipFile)) return;
+		const td = new TextDecoder;
+		const zip = fs.readFileSync(zipFile).buffer;
 		try {
-			fs.mkdirSync(resourcesLocation, {recursive: true});
+			const array = new Uint8Array(zip);
+			const data = new DataView(zip);
+			const offEOCD = array.findLastIndex(
+				(e, i, a) =>
+					e === 0x50 &&
+					a[i + 1] === 0x4b &&
+					a[i + 2] === 0x05 &&
+					a[i + 3] === 0x06
+			);
+			const recordCount = data.getUint16(offEOCD + 10, true);
+			for (let i = 0, pos = data.getUint32(offEOCD + 16, true); i < recordCount; i++) {
+				const n = data.getUint16(pos + 28, true);
+				const filePath = td.decode(array.subarray(pos + 46, pos + 46 + n));
+				if (!filePath.endsWith("/")) {
+					const absPath = path.join(dest, filePath);
+					fs.mkdirSync(path.dirname(absPath), {recursive: true});
+					const h = data.getUint32(pos + 42, true);
+					const contentStart = h + 30 + n + data.getUint16(h + 28, true);
+					const compressedContent = array.subarray(contentStart, contentStart + data.getUint32(pos + 20, true));
+					fs.writeFileSync(absPath, data.getUint16(h + 8, true) === 0 ? compressedContent : zlib.inflateRawSync(compressedContent));
+				}
+
+				pos += 46 + n + data.getUint16(pos + 30, true) + data.getUint16(pos + 32, true);
+			}
 		} catch (e) {
-			log.error(e);
+			console.error(e);
 		}
-		return resourcesLocation;
 	}
 
 	function getHomeDirectory() {
-		let home = process.env.WAKATIME_HOME;
-		if (home && home.trim() && fs.existsSync(home.trim()))
-			return home.trim();
+		let home = process.env.WAKATIME_HOME?.trim();
+		if (home && fs.existsSync(home)) return home;
 		return process.env[isWindows() ? "USERPROFILE" : "HOME"] || "";
-	}
-
-	function getCliLocation() {
-		return path.join(
-			getResourcesLocation(),
-			`wakatime-cli-${isWindows() ? "windows" : process.platform}-${architecture()}${isWindows() ? ".exe" : ""}`
-		);
 	}
 })();
